5f06fbb2321f03780d237a75872a04a1a3d88018
walebign3 <walebign3@gmail.com>
2022-11-12 10:56:20 +0300

    asr

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-#A script that starts a Flask web application:
+"""
+A script that starts a Flask web application:
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
+"""display Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
